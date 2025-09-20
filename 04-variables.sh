e7424f5424176a22ac2cfad40f5bd8bba7ceb4d2
Anil1931 <hanshpappa31@gmail.com>
2025-09-20 19:28:15 +0530

    Variables

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1:: Thanks for welcoming to Alaksa $PERSON2"
+echo "$PERSON2:: Thanks for Welcoming to USA $PERSON1"
+echo "$PERSON1:: How the things are going on $PERSON2"
+echo "$PERSON2:: $PERSON1 Expect you all good"
