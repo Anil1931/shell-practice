b3ab42c45a74eb75736ea0d6d5f680633ab765e4
Anil1931 <hanshpappa31@gmail.com>
2025-09-23 06:23:11 +0530

    variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-echo "Timestamp executed: $DATE"
+START_TIME=$(date +%s)
+
+sleep 10  # if $ this symbol is there it will be run in back ground
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo "Timestamp executed: $TOTAL_TIME"
