f0b8a8f28a1cb019700db863f824dee50595e32d
Anil1931 <hanshpappa31@gmail.com>
2025-09-23 18:40:34 +0530

    Conditions

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,13 +4,16 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: please runt this script with root privilage"
+    exit 1 # failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then 
     echo "ERROR:: instaling mysql is failure"
+    exit 1
 else
     echo " Installing mysql is success"
+    exit 0
 
 fi
