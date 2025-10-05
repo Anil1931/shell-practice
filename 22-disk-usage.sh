1c9024f9b7663350895b88f6ef5aa9938834fddd
Anil1931 <hanshpappa31@gmail.com>
2025-10-05 22:19:18 +0530

    CPU usage

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -13,7 +13,7 @@ do
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
 
-        MESSAGE="High disk usage on $PARTITION: $USAGE %"
+        MESSAGE+="High disk usage on $PARTITION: $USAGE %"
 
     fi
 
